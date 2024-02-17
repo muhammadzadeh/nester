9d99b0928158fe358400631dd58a32c4cea6fb68
Emir Muhammadzadeh <amir.msc90@gmail.com>
2024-02-17 15:55:42 +0330

    pipes and interceptor added

--- src/common/application/pipes.ts
+++ src/common/application/pipes.ts
@@ -1,8 +1,21 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ArgumentMetadata, INestApplication, Injectable, PipeTransform, ValidationPipe } from '@nestjs/common';
 import { ValidationException } from '../exception';
+import { camelCaseObject } from '../utils';
+
+@Injectable()
+class CamelCasePipe implements PipeTransform<any> {
+  transform(value: any, _: ArgumentMetadata) {
+    if (!value) {
+      return value;
+    }
+
+    return camelCaseObject(value);
+  }
+}
 
 export default (app: INestApplication): void => {
   app.useGlobalPipes(
+    new CamelCasePipe(),
     new ValidationPipe({
       transform: true,
       whitelist: true,
